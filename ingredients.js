da903f9b078c9743411ba8436939ae0af575ddbf
farnaz <farnaz.rashidi@gmail.com>
2017-04-10 03:29:19 +0000

    Add loop to print ingredients

---
+++ ingredients.js
@@ -0,0 +1,13 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+var count = 0
+while (count < ingredients.length) {
+  console.log(ingredients[count])
+  count++
+}
+for (i=0;i<ingredients.length;i++) {
+  console.log(ingredients[i]);
+}
+
+for (j=ingredients.length-1;j>=0;j--) {
+  console.log(ingredients[j]);
+}
